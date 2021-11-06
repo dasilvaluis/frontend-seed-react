6937722f863b942baf2012e24bbeb217ecf09639
Luis Silva <luismiguel.s91@gmail.com>
2021-11-06 23:33:23 +0200

    Add optimizations and browserslist

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+function renderApp() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+}
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept('./app.jsx', () => {
+    renderApp();
+  });
+}
